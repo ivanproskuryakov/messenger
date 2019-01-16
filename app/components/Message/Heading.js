fc93f4a9b02e545e353a8c6203f2df69d2c34897
Ivan Proskuryakov <volgodark@gmail.com>
2019-01-16 16:31:43 +0300

    Tuning UI, p4

--- app/components/Message/Heading.js
+++ app/components/Message/Heading.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar/Avatar';
 import Search from '@material-ui/icons/Search';
 import Close from '@material-ui/icons/Close';
 
-import Moment from 'react-moment';
-import { connect } from 'react-redux';
+import Tune from '@material-ui/icons/Tune';
+import Help from '@material-ui/icons/Help';
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MoreVert from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton/IconButton';
+
 import { calendarStringsHeader } from '../../service/time';
+import config from '../../config';
 
 const styles = ({
   icon: {
@@ -27,6 +39,27 @@ const styles = ({
 class Heading extends React.Component {
   state = {
     search: '',
+    anchorEl: null,
+  };
+
+  closeMenu = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  showMenu = (event) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  editSettings = () => {
+    location.href = config.URL_SETTINGS;
+  };
+
+  logout = () => {
+    location.href = config.URL_LOGOUT;
+  };
+
+  viewHelp = () => {
+    location.href = config.URL_HELP;
   };
 
   onClearClick = () => {
@@ -39,7 +72,7 @@ class Heading extends React.Component {
 
   render() {
     const { selected, classes } = this.props;
-    const { search } = this.state;
+    const { search, anchorEl } = this.state;
 
     if (!selected.name) {
       return '';
@@ -70,8 +103,46 @@ class Heading extends React.Component {
               className="clearIcon"
               onClick={this.onClearClick}
             />
-          ) : ('') }
+          ) : ('')}
         </div>
+
+        <IconButton
+          id="profileInfo"
+          className={classes.button}
+          component="span"
+          aria-label="Delete"
+          aria-owns={anchorEl ? 'profileMenu' : undefined}
+          aria-haspopup="true"
+          onClick={this.showMenu}
+        >
+          <MoreVert />
+        </IconButton>
+
+        <Menu
+          id="profileMenu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.closeMenu}
+        >
+          <MenuItem onClick={this.viewHelp}>
+            <ListItemIcon className={classes.icon}>
+              <Help />
+            </ListItemIcon>
+            <ListItemText classes={{ primary: classes.primary }} primary="Help" />
+          </MenuItem>
+          <MenuItem onClick={this.editSettings}>
+            <ListItemIcon className={classes.icon}>
+              <Tune />
+            </ListItemIcon>
+            <ListItemText classes={{ primary: classes.primary }} primary="Settings" />
+          </MenuItem>
+          <MenuItem onClick={this.logout}>
+            <ListItemIcon className={classes.icon}>
+              <ExitToApp />
+            </ListItemIcon>
+            <ListItemText classes={{ primary: classes.primary }} inset primary="Sign Out" />
+          </MenuItem>
+        </Menu>
       </div>
     );
   }
--- app/components/User/Me.js
+++ app/components/User/Me.js
@@ -4,24 +4,8 @@ import { connect } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar/Avatar';
 import { withStyles } from '@material-ui/core';
 
-import Tune from '@material-ui/icons/Tune';
-import Help from '@material-ui/icons/Help';
-import ExitToApp from '@material-ui/icons/ExitToApp';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MoreVert from '@material-ui/icons/MoreVert';
-import IconButton from '@material-ui/core/IconButton/IconButton';
-
-import config from '../../config';
-
 const styles = ({
   icon: {
-    margin: 0,
-  },
-  button: {
-    padding: 5,
     margin: 0,
   },
   avatar: {
@@ -32,33 +16,8 @@ const styles = ({
 });
 
 class Me extends React.Component {
-  state = {
-    anchorEl: null,
-  };
-
-  closeMenu = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  showMenu = (event) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  editSettings = () => {
-    location.href = config.URL_SETTINGS;
-  };
-
-  logout = () => {
-    location.href = config.URL_LOGOUT;
-  };
-
-  viewHelp = () => {
-    location.href = config.URL_HELP;
-  };
-
   render() {
     const { me, classes } = this.props;
-    const { anchorEl } = this.state;
 
     return (
       <div id="myProfile">
@@ -69,41 +28,6 @@ class Me extends React.Component {
         <div className="name">
           {me.name}
         </div>
-        <IconButton
-          className={`info ${classes.button}`}
-          component="span"
-          aria-label="Delete"
-          aria-owns={anchorEl ? 'profileMenu' : undefined}
-          aria-haspopup="true"
-          onClick={this.showMenu}
-        >
-          <MoreVert />
-        </IconButton>
-        <Menu
-          id="profileMenu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.closeMenu}
-        >
-          <MenuItem onClick={this.viewHelp}>
-            <ListItemIcon className={classes.icon}>
-              <Help />
-            </ListItemIcon>
-            <ListItemText classes={{ primary: classes.primary }} primary="Help" />
-          </MenuItem>
-          <MenuItem onClick={this.editSettings}>
-            <ListItemIcon className={classes.icon}>
-              <Tune />
-            </ListItemIcon>
-            <ListItemText classes={{ primary: classes.primary }} primary="Settings" />
-          </MenuItem>
-          <MenuItem onClick={this.logout}>
-            <ListItemIcon className={classes.icon}>
-              <ExitToApp />
-            </ListItemIcon>
-            <ListItemText classes={{ primary: classes.primary }} inset primary="Sign Out" />
-          </MenuItem>
-        </Menu>
       </div>
     );
   }
