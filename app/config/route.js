07d799e7b47a63ca1908d5e74aa9e7a544504f0f
Ivan Proskuryakov <volgodark@gmail.com>
2019-01-28 16:40:26 +0300

    mocking api

--- app/config/route.js
+++ app/config/route.js
@@ -8,8 +8,10 @@ const route = {
   URL_HELP: `${host}/help/z78`,
   URL_PROFILE: `${host}/profile`,
   URL_USER_AUTHORIZE_SUCCESS: `${host}/api/user/me`,
+
   URL_PUSHER_AUTH: `${host}/api/messenger/pusher/authPresence`,
   URL_PUSHER_ONLINE: `${host}/api/messenger/pusher/authOnline`,
+
   URL_ROOM: `${host}/api/messenger/room`,
   URL_MESSAGE_SEND_USER: `${host}/api/messenger/message/user`,
   URL_MESSAGE_READ: `${host}/api/messenger/message`,
